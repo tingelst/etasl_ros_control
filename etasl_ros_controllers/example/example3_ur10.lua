da20b1583d1ef493981413e4664280d241544065
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-02-21 13:10:37 +0100

    Added example using ur10

--- etasl_ros_controllers/example/example3_ur10.lua
+++ etasl_ros_controllers/example/example3_ur10.lua
@@ -3,7 +3,7 @@ require("geometric")
 
 local u = UrdfExpr()
 u:readFromParam("/robot_description")
-u:addTransform("ee", "tool0", "base_link")
+u:addTransform("ee", "ee_link", "base_link")
 
 local r = u:getExpressions(ctx)
 
@@ -11,12 +11,12 @@ local r = u:getExpressions(ctx)
 robot_ee = r.ee
 -- The name of the robot joints
 robot_joints = {
-    "joint_a1",
-    "joint_a2",
-    "joint_a3",
-    "joint_a4",
-    "joint_a5",
-    "joint_a6"
+    "shoulder_pan_joint",
+    "shoulder_lift_joint",
+    "elbow_joint",
+    "wrist_1_joint",
+    "wrist_2_joint",
+    "wrist_3_joint"
 }
 
 j1 = ctx:getScalarExpr(robot_joints[1])
@@ -26,7 +26,7 @@ j4 = ctx:getScalarExpr(robot_joints[4])
 j5 = ctx:getScalarExpr(robot_joints[5])
 j6 = ctx:getScalarExpr(robot_joints[6])
 
-maxvel = 1.0
+maxvel = 0.3
 for i = 1, #robot_joints do
     BoxConstraint{
         context = ctx,
@@ -40,8 +40,6 @@ tgt_x = ctx:createInputChannelScalar("tgt_x", 0.5)
 tgt_y = ctx:createInputChannelScalar("tgt_y", 0)
 tgt_z = ctx:createInputChannelScalar("tgt_z", 0)
 
-tgt_pose = ctx:createInputChannelFrame("tgt_pose")
-
 d = Variable{context = ctx, name = "d", vartype = "feature"}
 
 Constraint{
@@ -49,7 +47,7 @@ Constraint{
     name = "laserdistance",
     expr = d,
     target_lower = 0.5,
-    target_upper = 0.9,
+    target_upper = 0.7,
     K = 4
 }
 
@@ -80,8 +78,3 @@ Constraint{
 ctx:setOutputExpression("error_x", coord_x(laserspot) - tgt_x)
 ctx:setOutputExpression("error_y", coord_y(laserspot) - tgt_y)
 ctx:setOutputExpression("error_z", coord_z(laserspot) - tgt_z)
-ctx:setOutputExpression("laser_x", coord_x(laserspot))
-ctx:setOutputExpression("laser_y", coord_y(laserspot))
-ctx:setOutputExpression("laser_z", coord_z(laserspot))
-
-ctx:setOutputExpression("out_pose", robot_ee)
